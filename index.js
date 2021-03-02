ea23ba7917b522b1937e0d1035a809f86e7d973b
purvankbhiwgade <bhiwgadepurvank@gmail.com>
2021-03-02 23:04:29 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SupportPage from './Support Page/SupportPage'
+
+const App = () => {
+    return <div><SupportPage /></div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
